8388213515c439a109be627ca4aee697f9915886
himchan <himchan9494@gmail.com>
2021-11-24 15:55:14 +0900

    feat: Obfuscationing Korean function
    Obfuscationing Korean function is done and connected with DOM
    
    TODO: Error handling for other character which is not korean

--- main.js
+++ main.js
@@ -157,40 +157,64 @@ const componentConversionTable = [
   },
 ];
 
-const extraEndSoundCandidates = [
-  "ㄱ",
-  "ㄲ",
-  "ㄳ",
-  "ㄴ",
-  "ㄵ",
-  "ㄶ",
-  "ㄷ",
-  "ㄹ",
-  "ㄺ",
-  "ㄻ",
-  "ㄼ",
-  "ㄽ",
-  "ㄾ",
-  "ㄿ",
-  "ㅀ",
-  "ㅁ",
-  "ㅂ",
-  "ㅄ",
-  "ㅅ",
-  "ㅆ",
-  "ㅇ",
-  "ㅈ",
-  "ㅊ",
-  "ㅋ",
-  "ㅌ",
-  "ㅍ",
-  "ㅎ",
-];
+const korean_combinationer = (kor) => {
+  // kor format
+  //     {
+  //     chosung: { index: cho, char: cho_list[cho] },
+  //     joongsung: { index: joong, char: joong_list[joong] },
+  //     jongsung: { index: jong, char: jong_list[jong] },
+  //   }
 
-const korean_combinationer = (char) => {};
+  // 변환 가능한 초성 리스트
+  const changable_choList =
+    componentConversionTable[0][kor.chosung.char].split("");
+
+  // 변환된 초성
+  const changed_cho =
+    changable_choList[Math.floor(Math.random() * changable_choList.length)];
+
+  // 변환 가능한 중성 리스트
+  const changable_joongList =
+    componentConversionTable[1][kor.joongsung.char].split("");
+
+  // 변환된 중성
+  const changed_joong =
+    changable_joongList[Math.floor(Math.random() * changable_joongList.length)];
+
+  // 종성의 경우에는 종성이 없을 경우, 종성이 있을 경우를 고려한다.
+  let changable_jongList;
+
+  if (kor.jongsung.char === "") {
+    changable_jongList = jong_list;
+  } else {
+    changable_jongList =
+      componentConversionTable[2][kor.jongsung.char].split("");
+  }
+
+  // 변환된 종성
+  const changed_jong =
+    changable_jongList[Math.floor(Math.random() * changable_jongList.length)];
+
+  // 변환된 문자들의 인덱스값
+  let cho_index, joong_index, jong_index;
+
+  cho_index = cho_list.findIndex((char) => char === changed_cho);
+  joong_index = joong_list.findIndex((char) => char === changed_joong);
+  jong_index = jong_list.findIndex((char) => char === changed_jong);
+
+  // 출력부분, String.fromCharCode(유니코드)
+  // 유니코드는 44032 + (초성 * 588) + (중성 * 28)) + 종성
+  // String.fromCharCode(44032)
+
+  return String.fromCharCode(
+    44032 + cho_index * 588 + joong_index * 28 + jong_index
+  );
+};
 
 const korean_spliter = (char) => {
+  // starting korean unicode
   const ga = 44032;
+
   let charUnicode = char.charCodeAt(0);
 
   charUnicode = charUnicode - ga;
@@ -208,22 +232,22 @@ const korean_spliter = (char) => {
 
 const korean_obfuscationer = (text) => {
   const charList = text.split("");
-  charList.forEach((char) => {
+
+  const conveted_list = charList.map((char) => {
     if (char === " ") {
       return " ";
     } else {
       const splited_korean = korean_spliter(char);
+      const conveted_korean = korean_combinationer(splited_korean);
+      console.log(conveted_korean);
+      return conveted_korean;
     }
   });
+  //   console.log(conveted_list);
+  return conveted_list;
 };
 
 btn.addEventListener("click", () => {
-  korean_obfuscationer(inputBox.value);
+  let output = korean_obfuscationer(inputBox.value);
+  outputBox.value = output.join("");
 });
-
-// 우선 초성, 중성, 종성의 내용을 담고있는 배열을 생성한다. 첫 번째 문자인 '가'의 유니코드 상수로 지정한다.
-// uni 변수는 인수로 받아들인 kor를 유니코드로 변환한다. uni 변수는 기존의 uni 값에서 '가'의 유니코그 값을
-// 뺀 것을 저장한다.
-
-// 한글 코드 = 44032 + (초성 * 588) + (중성 * 28) + 종성
-// Math.floor((Math.random()*list.length))
